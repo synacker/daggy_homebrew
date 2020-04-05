d7bececa64175f6d32e95a10cc7810c30a203e27
synacker <milovidovmikhail@gmail.com>
2020-04-05 19:14:17 +0300

    Update to 2.0.0 version

--- daggy.rb
+++ daggy.rb
@@ -1,20 +1,23 @@
 class Daggy < Formula
-  desc "Data Aggregation Utility for aggregation data from your network"
+  desc "Daggy - Data Aggregation Utility. Open source, free, cross-platform, server-less and useful utility for data aggregation and streaming"
   homepage "https://daggy.dev"
-  url "https://github.com/synacker/daggy/archive/1.1.3/daggy-1.1.3.tar.gz"
-  sha256 "559a9d7916988edf67be50625d89b38c311edaa322c3238e0107883dc315f0c6"
+  url "https://github.com/synacker/daggy/archive/2.0.0.zip"
+  sha256 "a0f23c181ca2211fbc1fecab6ee5bfdf391dcd845a02a36dde5c8e5c79d9aaa3"
 
-  depends_on "botan"
+  depends_on "cmake" => :build
+  depends_on "wget" => :build
   depends_on "qt"
   depends_on "yaml-cpp"
+  depends_on "libssh2"
 
   def install
-    system "#{Formula["qt"].bin}/qmake", "PREFIX=#{prefix}", "CONFIG+=release", "VERSION=1.1.3", "BUILD_NUMBER=0"
-    system "make"
-    system "make", "install"
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "cmake", "src", "-DVERSION=2.0.0"
+    system "cmake", "--build", "."
   end
 
   test do
-    assert_match "daggy", shell_output("#{bin}/daggy -v")
+    assert_match "daggy 2.0.0", shell_output("#{bin}/daggy -v")
   end
 end
