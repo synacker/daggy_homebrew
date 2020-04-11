a9690e99ed6b87e0e0c0a164d80eee5ab0092035
synacker <milovidovmikhail@gmail.com>
2020-04-11 18:08:36 +0300

    Update version

--- daggy.rb
+++ daggy.rb
@@ -1,7 +1,7 @@
 class Daggy < Formula
   desc "Daggy - Data Aggregation Utility. Open source, free, cross-platform, server-less and useful utility for data aggregation and streaming"
   homepage "https://daggy.dev"
-  url "https://github.com/synacker/daggy/archive/2.0.0.zip"
+  url "https://github.com/synacker/daggy/archive/2.0.1.zip"
   sha256 "a0f23c181ca2211fbc1fecab6ee5bfdf391dcd845a02a36dde5c8e5c79d9aaa3"
 
   depends_on "cmake" => :build
@@ -19,12 +19,12 @@ class Daggy < Formula
     system "wget", "https://raw.githubusercontent.com/kainjow/Mustache/master/mustache.hpp", "-O", "src/Daggy/mustache.hpp"
     system "mkdir", "build"
     Dir.chdir('build')
-    system "cmake", "-D", "VERSION=2.0.0", "-D", "CMAKE_INSTALL_PREFIX:PATH=#{prefix}", "-D", "CMAKE_BUILD_TYPE=Release", "../src"
+    system "cmake", "-D", "VERSION=2.0.1", "-D", "CMAKE_INSTALL_PREFIX:PATH=#{prefix}", "-D", "CMAKE_BUILD_TYPE=Release", "../src"
     system "make", "all"
     system "make", "install"
   end
 
   test do
-    assert_match "daggy 2.0.0", shell_output("#{bin}/daggy -v")
+    assert_match "daggy 2.0.1", shell_output("#{bin}/daggy -v")
   end
 end
