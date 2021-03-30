6ab68f0ad814d1fae9af29ee002d5403ee7eda08
synacker <milovidovmikhail@gmail.com>
2021-03-30 14:02:32 -0700

    Fixed formula errors

--- daggy.rb
+++ daggy.rb
@@ -1,12 +1,12 @@
 class Daggy < Formula
   desc "Daggy - Data Aggregation Utility. Open source, free, cross-platform, server-less and useful utility for data aggregation and streaming"
   homepage "https://daggy.dev"
-  url "https://github.com/synacker/daggy/archive/2.0.1.zip"
-  sha256 "96b81c1f7e85820cc1b3e1807d40ba14e4581305416ca3d9c607f587a23b7b47"
+  url "https://github.com/synacker/daggy/archive/2.0.2.zip"
+  sha256 "ac29f20ea11ce561ed1bc8c46ae15fca1628ee3032872b7eb5274e40931d36d5"
 
   depends_on "cmake" => :build
   depends_on "wget" => :build
-  depends_on "qt"
+  depends_on "qt5"
   depends_on "yaml-cpp"
   depends_on "libssh2"
 
@@ -19,12 +19,12 @@ class Daggy < Formula
     system "wget", "https://raw.githubusercontent.com/kainjow/Mustache/master/mustache.hpp", "-O", "src/Daggy/mustache.hpp"
     system "mkdir", "build"
     Dir.chdir('build')
-    system "cmake", "-D", "VERSION=2.0.1", "-D", "CMAKE_INSTALL_PREFIX:PATH=#{prefix}", "-D", "CMAKE_BUILD_TYPE=Release", "../src"
+    system "cmake", "-D", "VERSION=2.0.2", "-D", "CMAKE_INSTALL_PREFIX:PATH=#{prefix}", "-D", "CMAKE_BUILD_TYPE=Release", "../src"
     system "make", "all"
     system "make", "install"
   end
 
   test do
-    assert_match "daggy 2.0.1", shell_output("#{bin}/daggy -v")
+    assert_match "daggy 2.0.2", shell_output("#{bin}/daggy -v")
   end
 end
